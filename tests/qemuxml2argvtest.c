c76ed92d112b87ba87004bf375179c08f52bfcd1
hhr293 <hengrui.hu@intel.com>
2022-08-02 02:51:42 +0800

    qemu: Add command-line to generate SGX EPC memory backend
    According to the result parsing from xml, add the argument of
    SGX EPC memory backend into QEMU command line.
    
    With NUMA config:
    
        #qemu-system-x86_64 \
            ...... \
            -object '{"qom-type":"memory-backend-epc","id":"memepc0","prealloc":true,"size":67108864,"host-nodes":[0,1],"policy":"bind"}' \
            -object '{"qom-type":"memory-backend-epc","id":"memepc1","prealloc":true,"size":16777216,"host-nodes":[2,3],"policy":"bind"}' \
            -machine sgx-epc.0.memdev=memepc0,sgx-epc.0.node=0,sgx-epc.1.memdev=memepc1,sgx-epc.1.node=1
    
    Without NUMA config:
    
        #qemu-system-x86_64 \
            ...... \
            -object '{"qom-type":"memory-backend-epc","id":"memepc0","prealloc":true,"size":67108864}' \
            -object '{"qom-type":"memory-backend-epc","id":"memepc1","prealloc":true,"size":16777216}' \
            -machine sgx-epc.0.memdev=memepc0,sgx-epc.1.memdev=memepc1
    
    Signed-off-by: Lin Yang <lin.a.yang@intel.com>
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -3486,6 +3486,9 @@ mymain(void)
 
     DO_TEST_CAPS_LATEST("devices-acpi-index");
 
+    DO_TEST_CAPS_VER("sgx-epc", "6.2.0");
+    DO_TEST_CAPS_LATEST("sgx-epc-numa");
+
     if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)
         virFileDeleteTree(fakerootdir);
 
